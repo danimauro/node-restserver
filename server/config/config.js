e62ce25f2516b19626beab04a61622d31c2b55bb
Daniel Mauricio <dmgutierrez7@misena.edu.co>
2018-08-21 17:26:40 -0500

    quitar comillas en el archivo config

--- server/config/config.js
+++ server/config/config.js
@@ -15,11 +15,9 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 // ============================
 let urlDB;
 
-/*if (process.env.NODE_ENV === 'dev') {
+if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
-} else {*/
-urlDB = 'mongodb://cafe-user:Mauro920328-@ds229552.mlab.com:29552/cafe';
-//}
+} else {
+    urlDB = 'mongodb://cafe-user:Mauro920328-@ds229552.mlab.com:29552/cafe';
+}
 process.env.URLDB = urlDB;
-
-//mongodb://cafe-user:<dbpassword>@ds229552.mlab.com:29552/cafe
